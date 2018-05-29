a8ffb16a45a08a204e5a4c5059be4e3ad90b63dd
imtianx <imtianx@gmail.com>
2018-05-29 21:43:59 +0800

    add app

---
+++ app/src/main/java/cn/imtianx/jetpacklearning/App.kt
@@ -0,0 +1,12 @@
+package cn.imtianx.jetpacklearning
+
+import android.app.Application
+
+/**
+ * <pre>
+ *     @desc:
+ * </pre>
+ * @author 奚岩
+ * @date 2018/5/29 下午2:51
+ */
+class App : Application()
