7b739107e0e253e493f06e20c249a592b97edf01
imtianx <imtianx@gmail.com>
2018-06-02 13:42:24 +0800

    use databinding

--- app/src/main/java/cn/imtianx/jetpacklearning/common/base/BaseDataBindingActivity.kt
+++ app/src/main/java/cn/imtianx/jetpacklearning/common/base/BaseDataBindingActivity.kt
@@ -1,23 +1,24 @@
-//package cn.imtianx.jetpacklearning.common.base
-//
-//import android.databinding.DataBindingUtil
-//import android.databinding.ViewDataBinding
-//
-///**
-// * <pre>
-// *     @desc: base DataBinding activity
-// * </pre>
-// * @author 奚岩
-// * @date 2018/5/29 下午3:24
-// */
-//abstract class BaseDataBindingActivity<T : ViewDataBinding> : BaseActivity() {
-//
-//    protected lateinit var binding: T
-//
-//    override fun initWindows() {
-//        super.initWindows()
-//        binding = DataBindingUtil.setContentView(this, getContentLayoutId())
-//    }
-//
-//
-//}
+package cn.imtianx.jetpacklearning.common.base
+
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+
+/**
+ * <pre>
+ *     @desc: base DataBinding activity
+ * </pre>
+ * @author 奚岩
+ * @date 2018/5/29 下午3:24
+ */
+abstract class BaseDataBindingActivity<T : ViewDataBinding> : BaseActivity() {
+
+    protected lateinit var binding: T
+
+    override fun initWindows() {
+        super.initWindows()
+        binding = DataBindingUtil.setContentView(this, getContentLayoutId())
+    }
+
+
+}
--- app/src/main/java/cn/imtianx/jetpacklearning/ui/main/MainFragment.kt
+++ app/src/main/java/cn/imtianx/jetpacklearning/ui/main/MainFragment.kt
@@ -1,10 +1,11 @@
 package cn.imtianx.jetpacklearning.ui.main
 
 import cn.imtianx.jetpacklearning.R
-import cn.imtianx.jetpacklearning.common.base.BaseFragment
+import cn.imtianx.jetpacklearning.common.base.BaseDataBindingFragment
 import cn.imtianx.jetpacklearning.common.extentions.getViewModel
+import cn.imtianx.jetpacklearning.databinding.MainFragmentBinding
 
-class MainFragment :BaseFragment() {
+class MainFragment : BaseDataBindingFragment<MainFragmentBinding>() {
 
     val viewModel by lazy {
         getViewModel(MainViewModel::class.java)
